401d2d7359492d3131015bb774aac9568d4fdb06
Muhammad Siddiq Shah <siddiqshah478@gmail.com>
2026-09-01 23:13:56 +0500

    Fix: remove nested git repo, treat client as normal folder

---
+++ client/app/(auth)/_layout.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function _layout() {
+  return (
+    <View>
+      <Text>Auth_layout</Text>
+    </View>
+  )
+}
---
+++ client/app/(tabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function _layout() {
+  return (
+    <View>
+      <Text>Tabs_layout</Text>
+    </View>
+  )
+}
